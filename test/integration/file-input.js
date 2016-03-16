9dfc66befb5c1e85fb6c58e7a11c67ed4b616412
Chun Yang <yang2007chun@gmail.com>
2016-03-16 15:56:48 -0500

    move test file to examples/test

--- test/integration/file-input.js
+++ test/integration/file-input.js
@@ -1,9 +1,14 @@
 /* eslint-env mocha */
 var fs = require('fs')
+var rimraf = require('rimraf')
 var assert = require('chai').assert
 var reacterminator = require('../../lib/index')
 
 describe('reacterminator with file input', function () {
+  beforeEach(function () {
+    rimraf.sync('./components')
+  })
+
   it('should generate one file with one component', function () {
     var ComponentA = `\
 class ComponentA extends React.Component {
@@ -17,7 +22,7 @@ export default ComponentA;\n`
     reacterminator(
       {
         type: 'path',
-        content: './examples/single-component.html'
+        content: './examples/test/component-a.html'
       },
       {
         generateFiles: true,
@@ -32,24 +37,68 @@ export default ComponentA;\n`
   })
 
   it('should emit an error when the input type is not allowed', function () {
-    assert(function () {
-      reacterminator(
-        {
-          type: 'non-exist',
-          content: './examples/single-component.html'
-        }
-      )
-    }, /the input.type should be file or string/)
+    assert.throws(
+      function () {
+        reacterminator(
+          {
+            type: 'non-exist',
+            content: './examples/test/component-a.html'
+          }
+        )
+      },
+      /the input.type should be file or string/
+    )
   })
 
   it('should emit an error when the input path does not exist', function () {
-    assert(function () {
-      reacterminator(
-        {
-          type: 'path',
-          content: './not-exist'
-        }
-      )
-    }, /is not a file or a directory/)
+    assert.throws(
+      function () {
+        reacterminator(
+          {
+            type: 'path',
+            content: './not-exist'
+          }
+        )
+      },
+      /no such file or directory/
+    )
+  })
+
+  it('should generate files for all the files in a folder non-recursivly', function () {
+    assert.throws(function () { fs.statSync('./components/ComponentA.jsx') })
+
+    reacterminator(
+      {
+        type: 'path',
+        content: './examples/test/'
+      },
+      {
+        generateFiles: true,
+        overrideFiles: true
+      }
+    )
+
+    assert(fs.statSync('./components/ComponentA.jsx').isFile())
+    assert.throws(function () { fs.statSync('./components/SubFolder.jsx') })
+  })
+
+  it('should generate files for all the files in a folder recursivly', function () {
+    assert.throws(function () { fs.statSync('./components/SubFolder.jsx') })
+    assert.throws(function () { fs.statSync('./components/ComponentA.jsx') })
+
+    reacterminator(
+      {
+        type: 'path',
+        content: './examples/test'
+      },
+      {
+        generateFiles: true,
+        overrideFiles: true,
+        recursive: true
+      }
+    )
+
+    assert(fs.statSync('./components/SubFolder.jsx').isFile())
+    assert(fs.statSync('./components/ComponentA.jsx').isFile())
   })
 })
