9a9aa0366f143741291725b60ab0cccc39f284bf
Matt <mwight1986@gmail.com>
2016-03-18 15:19:38 -0500

    recognize props and state

--- test/integration/state.js
+++ test/integration/state.js
@@ -2,26 +2,30 @@
 var assert = require('chai').assert
 var reacterminator = require('../../lib/index')
 
-describe.skip('reacterminator', function () {
-  it('should destruct state', function () {
+describe('reacterminator', function () {
+  it.only('should destruct state', function () {
     var content = `\
 <div data-component-name="ComponentA" data-component-state="firstName, lastName">
 </div>`
-    var ComponentA = `\
+    var expected = `\
 class ComponentA extends React.Component {
   render() {
     const {firstName, lastName} = this.state;
+
     return (
-      <div></div>
-    );
+      <div>
+      </div>
+      );
   }
 }
 
-export default ComponentA;`
+export default ComponentA;\n`
 
-    assert.deepEqual(
-      reacterminator({type: 'string', content: content})['ComponentA'],
-      ComponentA
-    )
+var actual = reacterminator({type: 'string', content: content})
+    .ComponentA
+    .formattedFileSnippet
+
+    console.log(reacterminator({type: 'string', content: content}).ComponentA.formattedFileSnippet)
+    assert.deepEqual(actual, expected)
   })
 })
