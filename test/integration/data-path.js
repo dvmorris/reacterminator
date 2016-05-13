f91c77fc29de769e0d51decfc97c077e74f9e981
Chun Yang <yang2007chun@gmail.com>
2016-05-13 13:39:03 -0500

    setup plugin system for App component

--- test/integration/data-path.js
+++ test/integration/data-path.js
@@ -1,53 +1,33 @@
 /* eslint-env mocha */
-var assert = require('chai').assert
-var reacterminator = require('../../lib/index')
+const assert = require('chai').assert
+const reacterminator = require('../../lib/index')
 
 describe('data-path', function () {
   it('should create containers and routes', function () {
-    var content = `\
+    const content = `\
 <div data-component-name="Login" data-component-path="login">
 </div>`
 
-    var LoginContainerExpected = `\
+    const AppExpected = `\
 import React from 'react';
-import { connect } from 'param-store';
-import Login from './Login';
 
-class LoginContainer extends React.Component {
-  render() {
-    const {path} = this.props.currentParams;
-    if (path !== 'login') {
-      return null;
-    }
-
-    return <Login {...this.props}/>;
-  }
-}
-
-export default connect(LoginContainer, 'path');\n`
-
-    var AppExpected = `\
-import React from 'react';
-import LoginContainer from './LoginContainer';
-
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
-      <div>
-        <LoginContainer/>
-      </div>
+      <div />
       );
   }
-}\n`
+}
+;
 
-    var components = reacterminator(
-      {type: 'string', content: content},
-      {changeLinksForParamStore: true}
+export default App;
+`
+
+    const components = reacterminator(
+      {type: 'string', content: content}
     )
-    var LoginContainerActual = components.LoginContainer.formattedFileSnippet
-    var AppActual = components.App.formattedFileSnippet
+    const AppActual = components.App.formattedFileSnippet
 
-    assert.deepEqual(LoginContainerActual, LoginContainerExpected)
     assert.deepEqual(AppActual, AppExpected)
   })
 })
