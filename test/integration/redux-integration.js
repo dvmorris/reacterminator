b2043d9d480a17a08af143e9859f376774b39809
Chun Yang <yang2007chun@gmail.com>
2016-06-06 11:14:58 -0500

    always prevent default event

--- test/integration/redux-integration.js
+++ test/integration/redux-integration.js
@@ -145,6 +145,8 @@ reduxExample
 import actionTypeConstants from '../../action-type-constants/index';
 
 export default function changeName(event) {
+  event.preventDefault();
+
   return {
     type: actionTypeConstants.reduxExample.changeName,
     value: event.target.value
